667a9035a417d2a1308a9d35db4c0993e0fa15f6
DevAzeredo <developer.azeredo@gmail.com>
2024-02-04 16:31:09 -0300

    Retirado conexao banco, topic dinamico

--- src/main/java/EstudosJava/RealTimeCHAT/Controller/ChatController.java
+++ src/main/java/EstudosJava/RealTimeCHAT/Controller/ChatController.java
@@ -1,28 +1,22 @@
 package EstudosJava.RealTimeCHAT.Controller;
 
 import EstudosJava.RealTimeCHAT.Model.Message;
-import EstudosJava.RealTimeCHAT.Model.mens;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.core.MessageSendingOperations;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 
-
-import java.util.List;
 @Controller
 public class ChatController {
+    @Autowired
+    private MessageSendingOperations<String> messagingTemplate;
 
     @MessageMapping("/sendMessage")
-    @SendTo("/topic/public")
     public Message sendMessage(@Payload Message chatMessage) {
-        System.out.println("Mensagem recebida do frontend: " + chatMessage.getContent());
-
-        // Lógica de processamento da mensagem
+        String chatCode = chatMessage.getChatId();
+        messagingTemplate.convertAndSend("/topic/"+chatCode, chatMessage);
         return chatMessage;
     }
-
 
 }
--- src/main/java/EstudosJava/RealTimeCHAT/Model/Message.java
+++ src/main/java/EstudosJava/RealTimeCHAT/Model/Message.java
@@ -1,20 +1,16 @@
 package EstudosJava.RealTimeCHAT.Model;
 
-import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
 @Builder
 public class Message {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
-    @ManyToOne
-    private Chat chatId;
+    private String sender;
+    private String chatId;
     private String content;
 }
+
 
